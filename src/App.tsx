41b566445015e98266f105a67d70b1abf4a852a3
Jairo Junior <jairojunior.png@gmail.com>
2023-12-30 22:35:34 -0300

    Criando e estilizando Textarea

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Button } from "./components/ui/button";
 import { Github } from 'lucide-react'
 import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
 
 export function App() {
   return ( 
-    <div>
+    <div className="min-h-screen flex flex-col">
       <div className="px-6 pv-5 flex items-center justify-between border-b">
         <h1 className="text-xl font-bold">upload.ai</h1>
         
         <div className="flex items-center gap-3"> 
-          <span className="text-sm text-nuted-foreground">
+          <span className="text-sm text-muted-foreground">
             Desenvolvido no NLW da Rocketseat
           </span>
           <Separator orientation="vertical" className="h-6"/>
@@ -19,6 +20,28 @@ export function App() {
           </Button>
         </div>
       </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              className="resize-none p-4 leading-relaxed" 
+              placeholder="Inclua o prompt para a IA"
+            />
+            <Textarea
+              className="resize-none p-4 leading-relaxed" 
+              placeholder="Resultado gerado pela IA" 
+              readOnly
+            />
+    
+          </div>
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável <code className="text-violet-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado
+          </p>
+        </div>
+        <aside className="w-80"></aside>
+      </main>
+
     </div>
   )
 }
