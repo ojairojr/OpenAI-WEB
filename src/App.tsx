029dcb103a5e6ec71d437ced75c51c340093c025
Jairo Junior <jairojunior.png@gmail.com>
2023-12-30 22:12:36 -0300

    Criando o topo da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import { Button } from "./components/ui/button";
+import { Github } from 'lucide-react'
+import { Separator } from "./components/ui/separator";
 
 export function App() {
-  return (
-    <Button> Teste  </Button>
+  return ( 
+    <div>
+      <div className="px-6 pv-5 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+        
+        <div className="flex items-center gap-3"> 
+          <span className="text-sm text-nuted-foreground">
+            Desenvolvido no NLW da Rocketseat
+          </span>
+          <Separator orientation="vertical" className="h-6"/>
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2"/>
+            GitHub
+          </Button>
+        </div>
+      </div>
+    </div>
   )
 }
