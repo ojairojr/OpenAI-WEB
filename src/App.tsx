1aa7ce901c1911a3cd308a5ea74cacde2288dba7
Jairo Junior <jairojunior.png@gmail.com>
2023-12-30 22:47:32 -0300

    Criando a input para o vídeo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Button } from "./components/ui/button";
-import { Github } from 'lucide-react'
+import { Github, FileVideo } from 'lucide-react'
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
 
@@ -39,7 +39,19 @@ export function App() {
             Lembre-se: você pode utilizar a variável <code className="text-violet-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado
           </p>
         </div>
-        <aside className="w-80"></aside>
+
+        <aside className="w-80 space-y-6">
+          <form className="space-y-6" >
+            <label 
+              htmlFor="video"
+              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+            >
+              <FileVideo className="w-4 h-4"/>
+              Selecione um vídeo
+            </label>
+            <input type="file" id="video" accept="video/mp4" className="sr-only"/>
+          </form>
+        </aside>
       </main>
 
     </div>
